651f4a9894c5e2051d107afacb27edcb19fc212e
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-01-02 12:30:15 +0100

    added methods for setting visibility of a button and blueprints for specific button classes

--- Buttons.py
+++ Buttons.py
@@ -22,9 +22,14 @@ class Button:
         self._width = width
         self._image = image
 
-        self._is_displayed = False
+        self._is_visible = False
+
+    def is_visible(self):
+        """returns true if button is displayed on screen"""
+        return self._is_visible
 
     def check_if_mouse_on_button(self, mouse_position):
+        """checks if mouse was on button"""
         mouse_x, mouse_y = mouse_position
         image_x, image_y = self._position  # position of top left corner
 
@@ -37,14 +42,36 @@ class Button:
             return True
         return False
 
+    def make_visible(self):
+        self._is_visible = True
+
+    def remove_from_screen(self):
+        self._is_visible = False
+
     def was_pressed(self, mouse_position):
-        pass
-        # check if button was visible
-        # check if start of click and end are on button
+        """returns True if button was pressed"""
+        if self.is_visible and self.check_if_mouse_on_button(mouse_position):
+            return True
+        return False
 
 
-# class EndButton(Button):
-#     def __init__(self):
-#         # load end button image
-#         # getting dimensions from image
-#         super().__init__()
+class PlayPVPButton(Button):
+    def __init__(self, position):
+        # loading button image
+        image = pygame.image.load("assets/Button.png")
+        # calculating dimensions from image
+        height = self._image.get_height()
+        width = self._image.get_width()
+        super().__init__(position=position, height=height, width=width, image=image)
+
+
+# class PlayPVCButton(Button):
+#     pass
+
+
+# class ExitButton(Button):
+#     pass
+
+
+# class ReplayButton(Button):
+#     pass
