fe85c9c9b915189c9e1a2ed43a2827f8eca68043
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-01-02 21:43:46 +0100

    added some tests for buttons

--- tests/test_Buttons.py
+++ tests/test_Buttons.py
@@ -12,6 +12,8 @@ class fake_image:
 def test_check_if_mouse_on_button():
     button = Button(position=(20, 30), screen="this is screen", image=fake_image)
     assert button.check_if_mouse_on_button((30, 40))
+    assert button.check_if_mouse_on_button((21, 32))
+    assert button.check_if_mouse_on_button((50, 60))
     assert not button.check_if_mouse_on_button((15, 15))
     assert not button.check_if_mouse_on_button((200, 15))
     assert not button.check_if_mouse_on_button((15, 200))
