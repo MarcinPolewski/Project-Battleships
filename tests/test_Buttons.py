7a68c7a64aed36e9a86331c29fbbeca296c04c2f
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-01-02 20:11:00 +0100

    fixed test for Buttons

--- tests/test_Buttons.py
+++ tests/test_Buttons.py
@@ -1,10 +1,16 @@
 from Buttons import Button
 
 
+class fake_image:
+    def get_height():
+        return 100
+
+    def get_width():
+        return 50
+
+
 def test_check_if_mouse_on_button():
-    button = Button(
-        position=(20, 30), height=100, width=50, image="for_testing_not_image"
-    )
+    button = Button(position=(20, 30), screen="this is screen", image=fake_image)
     assert button.check_if_mouse_on_button((30, 40))
     assert not button.check_if_mouse_on_button((15, 15))
     assert not button.check_if_mouse_on_button((200, 15))
@@ -12,9 +18,7 @@ def test_check_if_mouse_on_button():
 
 
 def test_check_if_mouse_on_button_corners():
-    button = Button(
-        position=(0, 0), height=100, width=50, image="for_testing_not_image"
-    )
+    button = Button(position=(0, 0), screen="this is screen", image=fake_image)
     assert button.check_if_mouse_on_button((0, 0))
     assert button.check_if_mouse_on_button((0, 100))
     assert button.check_if_mouse_on_button((50, 100))
